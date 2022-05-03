ba7a4aa494424db1550764b3e7ea62c206801dd0
Cristian Cernalevschi <cristi0235@gmail.com>
2022-05-03 11:45:59 +0300

    changed ReLu activation logic - overflow

--- rtl/relu_activation.sv
+++ rtl/relu_activation.sv
@@ -6,13 +6,13 @@
 
 module relu_activation #(
     parameter dataWidth = 16,
-    parameter intPart   = 4
+    parameter intWidth  = 4
 ) (
-    input                               clk        ,
-    input                               rst_n      ,
-    input                               relu_valid ,
-    input signed [2*dataWidth - 1:0]    relu_in    ,
-    output reg signed [dataWidth - 1:0] relu_out
+    input                         clk        ,
+    input                         rst_n      ,
+    input                         relu_valid ,
+    input  [2*dataWidth - 1:0]    relu_in    ,
+    output reg  [dataWidth - 1:0] relu_out
 );
 
 localparam minusonetenth = 1.0**-1.0;
@@ -22,7 +22,10 @@ always @(posedge clk) begin
         relu_out <= 0;
     else if (relu_valid)
         if ($signed(relu_in) >= 0)
-            relu_out <= relu_in[2*dataWidth-1-intPart-:dataWidth];
+            if(|relu_in[2*dataWidth-1-:intWidth+1]) //over flow to sign bit of integer part
+                relu_out <= {1'b0, {(dataWidth-1){1'b1}}}; //positive saturate
+            else
+                relu_out <= relu_in[2*dataWidth-1-intWidth-:dataWidth];
         else
             relu_out <= 0;
 end
