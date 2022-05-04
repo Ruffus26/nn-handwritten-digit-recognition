a667f3670154bc0029a449adf8eacca64ae11266
Cristian Cernalevschi <cristi0235@gmail.com>
2022-05-04 21:20:37 +0300

    channged NN architecture

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def main():
     # Get data from MNIST dataset
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-    # Create the Neural Network with 784 input neurons, 16 hidden neurons and 10 output neurons
-    net = network.Network([784, 16, 10])
+    # Create the Neural Network
+    net = network.Network([784, 30, 30, 10, 10])
 
     # Use stochastic gradient descent to learn from the MNIST training_data
-    net.SGD(training_data, 30, 100, 1.5, test_data=test_data)
+    net.SGD(training_data, 50, 100, 1.0, test_data=test_data)
 
     # Save data from trained network
     net.save("Weights.txt", "Biases.txt")
