a3e84b098c62d1f61e572b353d44d167dc1ff3f1
Cristian Cernalevschi <cristi0235@gmail.com>
2022-05-04 21:24:42 +0300

    enhanched structure

--- rtl/relu_activation.sv
+++ rtl/relu_activation.sv
@@ -15,15 +15,13 @@ module relu_activation #(
     output reg  [dataWidth - 1:0] relu_out
 );
 
-localparam minusonetenth = 1.0**-1.0;
-
 always @(posedge clk) begin
     if (~rst_n)
         relu_out <= 0;
     else if (relu_valid)
         if ($signed(relu_in) >= 0)
-            if(|relu_in[2*dataWidth-1-:intWidth+1]) //over flow to sign bit of integer part
-                relu_out <= {1'b0, {(dataWidth-1){1'b1}}}; //positive saturate
+            if(|relu_in[2*dataWidth-1-:intWidth + 1])
+                relu_out <= {1'b0, {(dataWidth - 1){1'b1}}};
             else
                 relu_out <= relu_in[2*dataWidth-1-intWidth-:dataWidth];
         else
